ee15f8f898759658739b2e186576f912d82fcfcd
YuweiZhan <winzhanyuwei@163.com>
2018-12-17 15:44:08 +0800

    defineVariables
    defineVariables

---
+++ scala/testscala/defineVariables.scala
@@ -0,0 +1,15 @@
+package testscala
+
+class defineVariables {
+
+
+}
+object defineVariables{
+  def main(args: Array[String]): Unit = {
+    val msg = "Hello world"
+    println(msg)
+    val msg2:String = "Hello again,world"
+    print(msg2)
+
+  }
+}
